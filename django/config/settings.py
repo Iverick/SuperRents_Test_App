972ac32bceba86a491ff492451e739c6c4de12df
Pawel Goloblia <iverick091@gmail.com>
2018-07-29 18:10:31 +0300

    Configured static files settings. Moved custom styles to a separate file within static folder.

--- django/config/settings.py
+++ django/config/settings.py
@@ -115,6 +115,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+
+# Static files directory
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 
 # REST Framework settings
